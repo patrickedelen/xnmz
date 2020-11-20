9f15dd045940c9cd2e0519acb058fa57a25c8973
Liam Osler <liam@builtrobotics.com>
2020-11-19 23:53:13 -0500

    catscii in pageContiner

--- pages/catscii.js
+++ pages/catscii.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 import styled from '@emotion/styled';
+import { PageColumn } from '../components/PageColumn';
 
 const Title = styled.h1`
   font-family: 'Lato', sans-serif;
@@ -30,18 +31,17 @@ const ProjectContainer = styled.div`
   margin: 20px 0;
 `;
 
-
 const Container = styled.div`
-@media (min-width: 768px) {
-  margin: 15vh 15vw;
-}
-@media (max-width: 767px) {
-  margin: 8vh 8vw;
-}
+  @media (min-width: 768px) {
+    margin: 15vh 15vw;
+  }
+  @media (max-width: 767px) {
+    margin: 8vh 8vw;
+  }
 `;
 
 const Underline = styled.a`
-  background-image: linear-gradient(90deg, rgba(255,191,113,1), rgba(255,145,153,1));
+  background-image: linear-gradient(90deg, rgba(255, 191, 113, 1), rgba(255, 145, 153, 1));
   background-position: 0 1.3em;
   background-repeat: repeat-x;
   background-size: 500% 4px;
@@ -78,38 +78,32 @@ function Catscii(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Container>
-        <Title><Underline href="/">xnmz.co</Underline>/catscii</Title>
+      <PageColumn>
+        <Title>
+          <Underline href="/">xnmz.co</Underline>/catscii
+        </Title>
         <Sub>
-        it gets lonely staring at a screen all day. thankfully i made this program that turns my cat pictures into ascii art that i can curl whenever the mood strikes.
+          it gets lonely staring at a screen all day. thankfully i made this program that turns my cat pictures into
+          ascii art that i can curl whenever the mood strikes.
         </Sub>
         <Sub>want to try it?</Sub>
         <Code>curl https://xnmz.co/api/cat/random</Code>
         <Sub>or click any of the pics to see the ascii version</Sub>
 
-      <CatContainer>
-        {
-          props.pics.map((pic) => {
+        <CatContainer>
+          {props.pics.map((pic) => {
             return (
               <CatPic key={pic._id}>
-                <a href={`/api/cat/${pic._id}`} >
-                  <RoundedImage
-                    src={pic.url}
-                    alt="A picture of a cat"
-                    width={200}
-                    height={200}
-                    layout="fixed"
-                  />
+                <a href={`/api/cat/${pic._id}`}>
+                  <RoundedImage src={pic.url} alt="A picture of a cat" width={200} height={200} layout="fixed" />
                 </a>
               </CatPic>
-              
-            )
-          })
-        }
-      </CatContainer>
-      </Container>
+            );
+          })}
+        </CatContainer>
+      </PageColumn>
     </div>
-  )
+  );
 }
 
 Catscii.getInitialProps = async (ctx) => {
@@ -117,6 +111,6 @@ Catscii.getInitialProps = async (ctx) => {
   const json = await res.json();
   // console.log(json[0].url);
   return { pics: json };
-}
+};
 
 export default Catscii;
